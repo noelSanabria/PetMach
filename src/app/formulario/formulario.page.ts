8e6791353c76edbc34f23413e507de52fe691876
Nicolas Chavez <nico.chavezo@duocuc.cl>
2023-09-07 04:03:47 -0300

    no funciona validacion formulario

--- src/app/formulario/formulario.page.ts
+++ src/app/formulario/formulario.page.ts
@@ -5,17 +5,48 @@ import {
   FormControl,
   Validators
 } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.page.html',
   styleUrls: ['./formulario.page.scss'],
 })
+
+
 export class FormularioPage implements OnInit {
 
-  constructor() { }
+  formulariocompatibilidad: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController:AlertController) {
+    this.formulariocompatibilidad = this.fb.group({
+      'nombre': new FormControl("", [Validators.required]),
+      'edad': new FormControl("", [Validators.required])
+    })
+     }
 
   ngOnInit() {
+  }
+  async ver() {
+    var f = this.formulariocompatibilidad.value;
+
+    if(this.formulariocompatibilidad.invalid){
+      const alert = await this.alertController.create({
+        header: 'Datos Incompletos',
+        message: 'Tienes que llenar todos los datos',
+        buttons: ['Aceptar']
+      })
+
+      await alert.present();
+      return;
+    }
+
+var compatibilidad = {
+  nombre: f.nombre,
+  edad: f.edad
+}
+
   }
 
 }
