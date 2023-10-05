4bd88eac514dab511ecd35b260f27859548eeae5
noelSanabria <no.sanabria@duocuc.cl>
2023-10-05 20:20:04 -0300

    CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,39 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [
+const routes: Routes = 
+
+[
+  
+
+  
+  {
+    path: 'product-add',
+    loadChildren: () => import('./producto/product-add/product-add.module').then( m => m.ProductAddPageModule)
+  },
+  {
+    path: 'product-list',
+    loadChildren: () => import('./producto/product-list/product-list.module').then( m => m.ProductListPageModule)
+  },
+  {
+    path: 'product-detail/:id',
+    loadChildren: () => import('./producto/product-detail/product-detail.module').then( m => m.ProductDetailPageModule)
+  },
+  {
+    path: 'product-edit/:id',
+    loadChildren: () => import('./producto/product-edit/product-edit.module').then( m => m.ProductEditPageModule)
+  },
+  {
+    path: 'product-all',
+    loadChildren: () => import('./producto/product-all/product-all.module').then( m => m.ProductAllPageModule)
+  },
+
+
+  
+
+
+
+
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import{ NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(),BrowserAnimationsModule, AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(),BrowserAnimationsModule, AppRoutingModule,HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
